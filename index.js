2e5c1ebe87c0567313407c50d44f0d2e555bfaad
Danipro2010YT <61289405+Danipro2010YT@users.noreply.github.com>
2020-06-11 11:39:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const request = require('request');
 
 
 
-const token = 'NzE1NTI0MzM0NTU0MzE2ODYw.Xs-flg.wfe_r35kHgbvlZOQBJUOWeoUl9o'
+const token = 'NzE1NTI0MzM0NTU0MzE2ODYw.XuIJ7Q.DJGcH4q0XOoNcIKcJZ9qMs2_Y2Y'
 
 const PREFIX = '!'
 
